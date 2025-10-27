ef374db625bc3f78ed62ee7a9e978eb206de0c85
Camila Volponi <camilavolponi19@gmail.com>
2025-10-27 16:17:19 -0300

    Ajustes criar pessoa doadora

--- PetDiverse/Models/PessoaDoadoraCadastroViewModel.cs
+++ PetDiverse/Models/PessoaDoadoraCadastroViewModel.cs
@@ -13,22 +13,35 @@ namespace PetDiverse.Models
             TipoFormaContato = new List<TipoFormaContato>();
         }
         public int Id { get; set; }
-        [Required(ErrorMessage ="Campo Obrigatório")] //colocar em todos, menos em ID
-        public string Nome { get; set; } 
+        [Required(ErrorMessage ="Campo Obrigatório")] 
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Telefone { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo Obrigatório")]
         [DisplayName("Estado")]
         public int IdEstado { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         [DisplayName("Cidade")]
         public int IdCidade { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         [DisplayName("Bairro")]
         public int IdBairro { get; set; }
-        public List<TipoFormaContato> TipoFormaContato { get; set; }
+        [MinLength(1, ErrorMessage = "Selecione pelo menos uma forma de contato")]
+        [DisplayName("Tipo forma de contato")]
+        public List<TipoFormaContato>? TipoFormaContato { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string CPF { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public DateTime DataNascimento { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string CNPJ { get; set; } = string.Empty;
-        public string Site { get; set; } 
-        public string RedeSocial { get; set; } 
-        public TipoPessoaCadastro TipoPessoaCadastro { get; set; }  
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Site { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string RedeSocial { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [DisplayName("Tipo de pessoa")]
+        public TipoPessoaCadastro? TipoPessoaCadastro { get; set; }  
     }
 
     public enum TipoPessoaCadastro
